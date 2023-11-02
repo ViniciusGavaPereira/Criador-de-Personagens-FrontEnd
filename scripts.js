6ac11a6591eaf696cc4a574a3e3c9ad418b32e05
Vinicius Gava <vini-gava@outlook.com.br>
2023-11-01 22:22:21 -0300

    Criacao dos scripts ppara conectar com o backend

--- scripts.js
+++ scripts.js
@@ -1,1 +1,40 @@
-alert("Funcionou")
+const formulario = document.querySelector("form");
+
+console.log(formulario);
+
+const Inome = document.querySelector(".nome");
+const Icpf = document.querySelector(".CPF");
+const Iemail= document.querySelector(".email");
+const Isenha = document.querySelector(".senha");
+const Isenha2 = document.querySelector(".senha2");
+
+formulario.addEventListener('submit', function (event){
+    event.preventDefault();
+
+    console.log(Inome.value);
+
+
+    cadastrar();
+
+});
+
+
+function cadastrar () {
+
+    fetch("http://localhost:8081/account/"), {
+        method: "POST",
+
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: Inome.value,
+            cpf: Icpf.value,
+            email: Iemail.value,
+            senha: Isenha.value
+        })
+        .then(function (res) {console.log(res)})
+        .catch(function (res) {console.log(res)})
+    }
+}
